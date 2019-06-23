da274f864ecea96e6c844a61ac21525c1497e02d
xiangping.hu <xiangping.hu@17zuoye.com>
2019-06-23 11:37:10 +0800

    延时操作

---
+++ Assets/Scripts/DelayTest.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DelayTest : MonoBehaviour
+{
+	Text txtLoading = null;
+	private sbyte countDownNum = 3;
+	private string[] dots = new string[]{".","..","..."};
+	private int index = 0;
+
+	void Awake()
+	{
+		txtLoading = gameObject.transform.Find("txtLoading").GetComponent<Text>();
+	}
+
+	void Start()
+	{
+		vp_Timer.In(1, CountDown,0);
+	}
+
+	private void CountDown(float deltaTime)
+	{
+		txtLoading.text = countDownNum--.ToString();
+		if (countDownNum <= 0)
+		{
+			vp_Timer.In(1, Loading, 0);
+			vp_Timer.Cancel(CountDown);
+		}
+	}
+
+	private void Loading(float deltaTime)
+	{
+		string str = dots[index++ % 3];
+		txtLoading.text = string.Format("Loading{0}", str);
+	}
+
+	void Update()
+	{
+
+	}
+
+	void OnDestroy()
+	{
+		vp_Timer.Cancel(Loading);
+	}
+}
